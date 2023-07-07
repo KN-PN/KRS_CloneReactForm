7aa6c21de0ab9036df1d6a1ac0733ebaa53dfb7e
KP.Xeer <killazaold1993@gmail.com>
2023-07-07 13:36:08 +0700

    delete comment

--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -13,7 +13,7 @@ function ListItem(props) {
                     items.map(({ name: title, amount }) => {
                         // items.map((props) => {
                         console.log('ListItem page');
-                        // console.log('props: ', props);
+                      
                         console.log('title: ', title);
                         console.log('name: ', name);
                         console.log('amount: ', amount);
