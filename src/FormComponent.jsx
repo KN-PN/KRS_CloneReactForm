4b7a73c7b5985ce0024db8784585befffc4891b5
KP.Xeer <killazaold1993@gmail.com>
2023-07-12 10:09:05 +0700

    remove unused and comment

--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -1,5 +1,3 @@
-// import React from 'react'
-// import React, { useState, useRef } from 'react';
 import { useRef } from 'react';
 import './FormComponent.css'
 
--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,4 +1,3 @@
-// import React from 'react'
 import PropTypes from 'prop-types';
 import Item from './Item'
 import { v4 as uuidv4 } from 'uuid';
